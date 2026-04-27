0742715852f7e0cf3754681e7d2245030a2205a6
Jona <jona@krampe.dev>
2026-04-27 16:23:54 +0200

    remove terminal and browser from autostart

--- hyprland/.config/hypr/hyprland.lua
+++ hyprland/.config/hypr/hyprland.lua
@@ -48,9 +48,7 @@ local browser = "zen-browser"
 -- Or execute your favorite apps at launch like this:
 --
 hl.on("hyprland.start", function()
-	hl.exec_cmd(terminal)
 	hl.exec_cmd("waybar & hyprpaper")
-	hl.exec_cmd(browser)
 	hl.exec_cmd("wl-paste --type text --watch cliphist store")
 	hl.exec_cmd("wl-paste --type image --watch cliphist store")
 end)
