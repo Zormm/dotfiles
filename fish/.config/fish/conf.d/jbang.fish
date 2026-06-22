bcf2eef60a6ede0a21fbdd680c54b0fd235a28b5
Jona <jona@krampe.dev>
2026-06-22 15:02:19 +0200

    add jbang to fish

---
+++ fish/.config/fish/conf.d/jbang.fish
@@ -0,0 +1,10 @@
+# Add JBang to environment
+
+abbr --add j! jbang
+
+fish_add_path /home/zormi/.jbang/bin
+
+set -gx JAVA_HOME /home/zormi/.jbang/currentjdk
+
+fish_add_path /home/zormi/.jbang/currentjdk/bin
+
