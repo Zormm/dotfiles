fd2e78f8e3a1801b4aded8c907f6bbd5db5e4aa6
Jona Krampe <jona@do-nb-081.ism.local>
2026-06-23 11:26:38 +0200

    added macos dotfiles

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,7 +3,11 @@ if status is-interactive
     set -g fish_greeting ''
 end
 
-export PATH="$HOME/.local/bin:$PATH"
+# export PATH="$HOME/.local/bin:$PATH"
+# export PATH="$HOME/bun/bin:$PATH"
+
+fish_add_path "$HOME/.bun/bin"
+fish_add_path "$HOME/.local/bin"
 
 zoxide init fish | source
 alias cd='z'
